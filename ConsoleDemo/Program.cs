15587727d276c8643e0649aa29ea01a03451aca8
Luis Inacio <lrs.inacio@gmail.com>
2021-05-05 18:43:52 +0100

    BlazorWasm and WebAPI demo

---
+++ ConsoleDemo/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Net.Http;
+using ConsoleDemo;
+
+swaggerClient client = new("https://localhost:44316", new HttpClient());
+var records = await client.WeatherForecastAsync();
+
+foreach (var r in records)
+{
+    Console.WriteLine($"{ r.Date }: { r.Summary }");
+}
